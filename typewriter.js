4f8426348c4cfedf2948fa0adeb89dca2f3087dd
veda1612 <vedavasavisiri@gmail.com>
2021-01-13 17:28:30 +0000

    typewriter.s added

--- typewriter.js
+++ typewriter.js
@@ -1,14 +1,12 @@
 const sentence = "hello there from lighthouse labs";
-let delay = 1000;
-setTimeout(() => {
-    // print the char here
-    //let delay = 1000;
-    for (const char of sentence) {
-        //process.stdout.write(char);
-        console.log(char);
-        
-        delay = delay + 5000;
-      }
-  }, delay);
+let delay = 0;  
+for (const char of sentence) {
+
+    setTimeout(() => {
+        process.stdout.write(char);
+    }, delay)
+    delay +=50;
+  };
 
 
+   
